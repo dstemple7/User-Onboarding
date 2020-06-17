9d78c72215b5ba498d455aef8fd36e3826ebe5df
David Temple <dstemple7@gmail.com>
2020-06-17 15:41:03 -0500

    form done

---
+++ src/Form.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export default function Form (props) { 
+  const {
+    values,
+    onSubmit,
+    onInputChange,
+    onCheckboxChange,
+    disabled,
+    errors,        
+  } = props
+    
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+          <h2>Add a User</h2>
+
+          <button disabled={disabled}>submit</button>
+
+          <div>
+              <div>{errors.name}</div>
+              <div>{errors.email}</div>
+              <div>{errors.password}</div>
+              <div>{errors.spiritAnimal}</div>
+              <div>{errors.tos}</div>
+          </div>
+      </div>
+
+      <div>
+        <h4>General Information</h4>
+
+        <label>Name&nbsp;
+            <input 
+                value={values.name}
+                onChange={onInputChange}
+                name='name'
+                type='text'
+            />
+        </label>
+
+        <label>Email&nbsp;
+            <input 
+                value={values.email}
+                onChange={onInputChange}
+                name='email'
+                type='email'
+            />
+        </label>
+
+        <label>Password&nbsp;
+            <input 
+                value={values.password}
+                onChange={onInputChange}
+                name='password'
+                type='password'
+            />
+        </label>
+
+        <label>Spirit Animal&nbsp;
+            <input 
+                value={values.spiritAnimal}
+                onChange={onInputChange}
+                name='spiritAnimal'
+                type='text'
+            />
+        </label>
+
+        <label>I Agree to the Legalese BS&nbsp;
+            <input 
+                value={values.tos}
+                onChange={onCheckboxChange}
+                name='tos'
+                type='checkbox'
+            />
+        </label>
+
+      </div>
+    </form>
+  )
+}
